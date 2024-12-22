61bef4d5eeeedfe1669ff50ef8684d64bc73a8e6
Adithya Bhat <dth.bht@gmail.com>
2024-12-21 19:41:48 -0800

    Update reliable_sender.rs
    Fixed the debug message.

--- src/plaintcp/reliable_sender.rs
+++ src/plaintcp/reliable_sender.rs
@@ -248,7 +248,7 @@ where
 
     async fn run(&mut self) 
     {
-        log::debug!("Running Connection Loop for {}", address);
+        log::debug!("Running Connection Loop for {}", self.address);
         let mut waiter = Waiter::new(self.retry_delay);
         loop {
             match TcpStream::connect(self.address).await {
